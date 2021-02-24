4d66dfc8a32b431c93b7e5cde7e7d0de6bf47b20
Priyank Patel <patel23priyank@gmail.com>
2021-02-24 15:22:12 +0530

    add components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { UhOhComponent } from './shared/components/uh-oh/uh-oh.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    UhOhComponent,
+    HomeComponent,
+    UserComponent,
+    PostComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/components/uh-oh/uh-oh.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uh-oh',
+  templateUrl: './uh-oh.component.html',
+  styles: []
+})
+export class UhOhComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
