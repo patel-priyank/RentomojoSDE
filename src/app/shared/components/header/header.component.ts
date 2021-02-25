101e916ec56977ae2922dbba847f45c684bdb92f
Priyank Patel <patel23priyank@gmail.com>
2021-02-25 11:23:41 +0530

    change anchor tag to angular router for header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../../services/app.service';
 import { ThemeService } from 'ng-bootstrap-darkmode';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +9,28 @@ import { ThemeService } from 'ng-bootstrap-darkmode';
   styles: []
 })
 export class HeaderComponent implements OnInit {
-  constructor(private themeService: ThemeService, private appService: AppService) {}
+  //#region Constructor
+
+  constructor(private themeService: ThemeService, private appService: AppService, private router: Router) {}
+
+  //#endregion
+
+  //#region Angular methods
 
   ngOnInit(): void {
     this.themeService.theme$.subscribe((theme) => {
       this.appService.switchColors();
     });
   }
+
+  //#endregion
+
+  //#region Public methods
+
+  // navigate to home page
+  public navigateToHome(): void {
+    this.router.navigate(['']);
+  }
+
+  //#endregion
 }
